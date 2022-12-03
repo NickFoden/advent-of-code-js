1ecb5630bc15a263fc9240eaf93d6d9e4c030cf2
Nick Foden <nickfoden@gmail.com>
2022-12-03 14:45:35 -0500

    Day3 (#4)
    * Day 3 part 1
    
    * Finish day 3

--- 2022/day3.spec.js
+++ 2022/day3.spec.js
@@ -1,75 +1,127 @@
 const fs = require("fs");
-const { parseInputIntoNumbersArray } = require("./common");
+const { parseInputIntoStringArray } = require("./common");
 
-const puzzleInput = fs.readFileSync(__dirname + "/inputs/1.txt", {
+const puzzleInput = fs.readFileSync(__dirname + "/inputs/3.txt", {
   encoding: "utf8",
 });
-const sampleInput = fs.readFileSync(__dirname + "/inputs/1sample.txt", {
+const sampleInput = fs.readFileSync(__dirname + "/inputs/3sample.txt", {
   encoding: "utf8",
 });
 
-const elfWithMostCalories = (food) => {
-  let mostCalories = 0;
-  let currentElfCalories = 0;
+const determinePriority = (item) => {
+  let result = 0;
+  const alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
 
-  food.forEach((calories, idx) => {
-    if (calories === 0 || idx === food.length - 1) {
-      if (currentElfCalories > mostCalories) {
-        mostCalories = currentElfCalories;
-      }
-      currentElfCalories = 0;
-    } else {
-      currentElfCalories += calories;
+  alphabet.every((alpha, idx) => {
+    if (item === alpha) {
+      result = idx + 1;
+      return false;
+    } else if (item === alpha.toUpperCase()) {
+      result = idx + 1 + 26;
+      return false;
     }
+    return true;
   });
-
-  return mostCalories;
+  return result;
 };
 
-const topThreeElves = (food) => {
-  let totalCalories = [0, 0, 0];
-  let currentElfCalories = 0;
+const ruckSackPriority = (packs) => {
+  let total = 0;
 
-  const final = food.length;
+  packs.forEach((pack) => {
+    const half = pack.length / 2;
+    const firstCompartment = pack.slice(0, half);
+    const secondCompartment = pack.slice(half, pack.length);
+    const duplicates = [];
 
-  for (let i = 0; i < final; i++) {
-    let calories = food[i];
-    if (calories !== 0) {
-      currentElfCalories += calories;
+    firstCompartment.split("").forEach((letter) => {
+      if (secondCompartment.includes(letter) && !duplicates.includes(letter)) {
+        duplicates.push(letter);
+      }
+    });
+
+    duplicates.forEach((l) => {
+      const val = determinePriority(l);
+      total += val;
+    });
+  });
+
+  return total;
+};
+
+const ruckSackBadges = (packs) => {
+  let result = 0;
+  let currentGroup = [];
+  let currentDuplicates = [];
+
+  for (let i = 0; i < packs.length; i++) {
+    currentGroup.push(packs[i]);
+
+    if (i % 3 === 2) {
+      currentGroup[0].split("").forEach((letter) => {
+        if (
+          currentGroup[1].includes(letter) &&
+          currentGroup[2].includes(letter) &&
+          !currentDuplicates.includes(letter)
+        ) {
+          currentDuplicates.push(letter);
+        }
+      });
+
+      result += determinePriority(currentDuplicates[0]);
+
+      currentDuplicates = [];
+      currentGroup = [];
     }
-
-    if (calories === 0 || i === final - 1) {
-      totalCalories.push(currentElfCalories);
-      currentElfCalories = 0;
-    }
-    totalCalories.sort((a, b) => (a > b ? -1 : 0));
-    totalCalories = totalCalories.slice(0, 3);
   }
 
-  return totalCalories.reduce((prev, next) => {
-    return prev + next;
-  }, 0);
+  return result;
 };
 
-describe("Day 1", () => {
+describe("Day 3", () => {
   test("part 1 sample", () => {
-    expect(
-      elfWithMostCalories(parseInputIntoNumbersArray(sampleInput))
-    ).toEqual(24000);
-  });
-  test("part 1 solve", () => {
-    expect(
-      elfWithMostCalories(parseInputIntoNumbersArray(puzzleInput))
-    ).toEqual(70116);
-  });
-  test("part 2 sample", () => {
-    expect(topThreeElves(parseInputIntoNumbersArray(sampleInput))).toEqual(
-      45000
+    expect(ruckSackPriority(parseInputIntoStringArray(sampleInput))).toEqual(
+      157
     );
   });
+  test("part 1 solve", () => {
+    expect(ruckSackPriority(parseInputIntoStringArray(puzzleInput))).toEqual(
+      8252
+    );
+  });
+  test("part 2 sample", () => {
+    expect(ruckSackBadges(parseInputIntoStringArray(sampleInput))).toEqual(70);
+  });
   test("part 2 solve", () => {
-    expect(topThreeElves(parseInputIntoNumbersArray(puzzleInput))).toEqual(
-      206582
+    expect(ruckSackBadges(parseInputIntoStringArray(puzzleInput))).toEqual(
+      2828
     );
   });
 });
