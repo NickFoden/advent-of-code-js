e173d8efc6c14511e24494ba4da2d66915a3a386
Nick Foden <nickfoden@gmail.com>
2022-12-03 14:22:03 -0500

    Day 2 (#3)

--- 2022/common/index.js
+++ 2022/common/index.js
@@ -1,6 +1,9 @@
 const parseInputIntoNumbersArray = (input) =>
   input.split("\n").map((i) => Number(i));
 
+const parseInputIntoStringArray = (input) => input.split("\n").map((i) => i);
+
 module.exports = {
   parseInputIntoNumbersArray,
+  parseInputIntoStringArray,
 };
